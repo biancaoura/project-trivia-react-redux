5f7fa76464c402e8dfc22b1cc48db3af04080058
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2022-09-13 19:08:38 -0300

    Requisito 19

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -2,15 +2,39 @@ import React, { Component } from 'react';
 import { shape } from 'prop-types';
 
 export default class Ranking extends Component {
+  constructor() {
+    super();
+    this.state = {
+      ranking: [],
+    };
+  }
+
+  componentDidMount() {
+    const ranking = JSON.parse(localStorage.getItem('ranking'));
+    const MINUS_ONE = -1;
+    const sortedRanking = ranking.sort((a, b) => (a.score > b.score ? MINUS_ONE : 1));
+    this.setState({ ranking: sortedRanking });
+  }
+
   handleClick = () => {
     const { history } = this.props;
     history.push('/');
   };
 
   render() {
+    const { ranking } = this.state;
     return (
       <div>
         <h1 data-testid="ranking-title">Ranking</h1>
+        {
+          ranking.map((position, index) => (
+            <section key={ position.picture }>
+              <img src={ position.picture } alt={ position.name } />
+              <h6 data-testid={ `player-name-${index}` }>{ position.name }</h6>
+              <h6 data-testid={ `player-score-${index}` }>{ position.score }</h6>
+            </section>
+          ))
+        }
         <button
           type="button"
           data-testid="btn-go-home"
